7072a7baa8098969959eb84db054432f5d464b09
Guilherme Bernardo <bguilherme51@gmail.com>
2021-06-15 18:51:27 -0400

    package, package-lock, npm install, uninstall, global install

--- JohnSmilga'sCourse/app.js
+++ JohnSmilga'sCourse/app.js
@@ -20,3 +20,24 @@ const _ = require('lodash')
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items) //used to transform this array of objects in an normal array
 console.log(newItems)
+console.log('hello people');
+
+/*
+ in a clone of this repository or any other with node package dependencies that aren't available you can install its dependencies through the command 
+ **npm install packageName**
+ and we have 
+ **npm install packageName -D**
+  with it, node will automatically install the project dependencies, as for the -D its for installing as a dev-dependency package
+ */
+
+/* what kind of dependencies do we install as dev dependencies? for example, testing packages, linting, etc etc. usually while we are creating the app, once its finished we just use it as dependency only, not dev-dep
+ */
+
+/*with scripts we can put the automatically restart the app without problems, in our case we are using nodemon to autorun the app, but we'll uninstall this package with *npm uninstall packageName**/
+
+//as for package-lock.json
+/* it is used to lock the version of the dependecies, and the packages of the dependecies so the project won't be buggy when a dependency version changes  */
+
+
+
+// as for the version in the package.json, it's represented as 3 different types of versions with 0.0.0, the first one is meant to be a change that cannot be backported, an enormous change in the package, se second 0 is for minor changes, that we can expect that your code will run with it even if this number changes, and the last one is for bug fixes.

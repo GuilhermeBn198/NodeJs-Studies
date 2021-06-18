f6a65e1a5ea50b435369d239368100ebb458a1ed
Guilherme Bernardo <bguilherme51@gmail.com>
2021-06-18 00:35:29 -0400

    event loop examples finished

--- JohnSmilga'sCourse/1-event-loop-examples/1-read-file.js
+++ JohnSmilga'sCourse/1-event-loop-examples/1-read-file.js
@@ -1,13 +1,13 @@
-const { readFile, writeFile } = require('fs')
+const { readFile } = require('fs')
 
 console.log('started a first task')
 // CHECK FILE PATH!!!!
-readFile('./content/first.txt', 'utf8', (err, result) => {
+readFile('./content/first.txt', 'utf8', (err, result) => { //callback
   if (err) {
     console.log(err)
     return
   }
   console.log(result)
   console.log('completed first task')
-})
+}) 
 console.log('starting next task')
